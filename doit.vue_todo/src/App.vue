d7f01113abb60d30b1c2ce41e15060e4ed96c7e4
hyony590 <126864361+hyony590@users.noreply.github.com>
2025-04-25 15:23:20 +0900

    Update App.vue
    11

--- doit.vue_todo/src/App.vue
+++ doit.vue_todo/src/App.vue
@@ -28,7 +28,7 @@ export default {
   },
   methods : {
     addTodo(todoItem) {
-      //로컬 스토리지에 데이터를 추가하는 로직
+      //로컬 스토리지에 데이터를 추가하는 로직1
       localStorage.setItem(todoItem, todoItem);
       this.todoItems.push(todoItem);
     },
